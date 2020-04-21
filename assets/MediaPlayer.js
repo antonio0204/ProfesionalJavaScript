a3fe7e0b6ac130daee017dff9650c2972cf0bf6e
ronald0204 <ronaldaguirre0204@gmail.com>
2020-04-21 11:09:36 -0500

    cambios para que el archivo se linkeara e iniciaria el video solo

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,21 +1,47 @@
 function MediaPlayer(config) {
-    this.media = config.el;
+  this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  });
+};
+
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+};
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function () {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
   }
+};
 
-  MediaPlayer.prototype.play = function() {
-    this.media.play();
-  };
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+};
 
-  MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-  };
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+};
 
-  MediaPlayer.prototype.togglePlay = function() {
-    if (this.media.paused) {
-      this.play();
-    } else {
-      this.pause();
-    }
-  };
+const video = document.querySelector('video');
+const player = new MediaPlayer({
+  el: video
+});
 
-  export default MediaPlayer;
+const button = document.querySelector('button');
+button.onclick = () => player.togglePlay();
+
+
+export default MediaPlayer;
